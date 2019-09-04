5d4ad75fdd2af11a3e5d177078fb3bf99904fb68
Alex Kube <alexander.j.kube@gmail.com>
2019-09-04 22:23:58 +0430

    Rename buffers to attachments

--- parser.go
+++ parser.go
@@ -37,45 +37,47 @@ type BinaryPlaceholder struct {
 	Number      int  `json:"num"`
 }
 
-func replaceByteArraysWithPlaceholders(data interface{}, buffers [][]byte) (interface{}, [][]byte) {
+func replaceByteArraysWithPlaceholders(data interface{}, attachments [][]byte) (interface{}, [][]byte) {
 	fmt.Println(reflect.TypeOf(data).Kind())
 	switch d := data.(type) {
+	// For []byte, generate a placeholder and append the []byte to the buffers array
 	case []byte:
-		placeholder := BinaryPlaceholder{true, len(buffers)}
-		buffers = append(buffers, d)
-		return placeholder, buffers
+		placeholder := BinaryPlaceholder{true, len(attachments)}
+		attachments = append(attachments, d)
+		return placeholder, attachments
+	// For basic value types, just return the value
 	case int:
-		return d, buffers
+		return d, attachments
 	case int8:
-		return d, buffers
+		return d, attachments
 	case int16:
-		return d, buffers
+		return d, attachments
 	case int32:
-		return d, buffers
+		return d, attachments
 	case int64:
-		return d, buffers
+		return d, attachments
 	case uint:
-		return d, buffers
+		return d, attachments
 	case uint8:
-		return d, buffers
+		return d, attachments
 	case uint16:
-		return d, buffers
+		return d, attachments
 	case uint32:
-		return d, buffers
+		return d, attachments
 	case uint64:
-		return d, buffers
+		return d, attachments
 	case float32:
-		return d, buffers
+		return d, attachments
 	case float64:
-		return d, buffers
+		return d, attachments
 	case bool:
-		return d, buffers
+		return d, attachments
 	case complex64: // TODO: Do these have a direct JS equivalent?
-		return d, buffers
+		return d, attachments
 	case complex128: // TODO: Do these have a direct JS equivalent?
-		return d, buffers
+		return d, attachments
 	case string:
-		return d, buffers
+		return d, attachments
 	default:
 		rt := reflect.TypeOf(data)
 		rv := reflect.ValueOf(data)
@@ -83,19 +85,19 @@ func replaceByteArraysWithPlaceholders(data interface{}, buffers [][]byte) (inte
 		switch rt.Kind() {
 		case reflect.Slice:
 			// If we receive a slice, loop through all the values and run the binary encoding method again
-			// This ensures that any array member that is a []byte has the []byte appended to the buffers list
+			// This ensures that any array member that is a []byte has the []byte appended to the attachments list
 			var encoded []interface{}
 			var encodedVal interface{}
 
 			for _, val := range data.([]interface{}) {
-				encodedVal, buffers = replaceByteArraysWithPlaceholders(val, buffers)
+				encodedVal, attachments = replaceByteArraysWithPlaceholders(val, attachments)
 				encoded = append(encoded, encodedVal)
 			}
 
-			return encoded, buffers
+			return encoded, attachments
 		case reflect.Array:
 			// If we receive an array, loop through all the values and run the binary encoding method again
-			// This ensures that any array member that is a []byte has the []byte appended to the buffers list
+			// This ensures that any array member that is a []byte has the []byte appended to the attachments list
 			var encoded []interface{}
 			var encodedVal interface{}
 
@@ -108,8 +110,8 @@ func replaceByteArraysWithPlaceholders(data interface{}, buffers [][]byte) (inte
 					binary = append(binary, rv.Index(i).Interface().(uint8))
 				}
 
-				encodedVal, buffers = replaceByteArraysWithPlaceholders(binary, buffers)
-				return encodedVal, buffers
+				encodedVal, attachments = replaceByteArraysWithPlaceholders(binary, attachments)
+				return encodedVal, attachments
 			}
 
 			var beforeEncoding []interface{}
@@ -120,14 +122,14 @@ func replaceByteArraysWithPlaceholders(data interface{}, buffers [][]byte) (inte
 			}
 
 			for _, val := range beforeEncoding {
-				encodedVal, buffers = replaceByteArraysWithPlaceholders(val, buffers)
+				encodedVal, attachments = replaceByteArraysWithPlaceholders(val, attachments)
 				encoded = append(encoded, encodedVal)
 			}
 
-			return encoded, buffers
+			return encoded, attachments
 		case reflect.Struct:
 			// If we receive a struct, loop through all the keys and run the binary encoding method again
-			// This ensures that any struct field that is a []byte has the []byte appended to the buffers list
+			// This ensures that any struct field that is a []byte has the []byte appended to the attachments list
 			// and the value replaced by a placeholder
 			encoded := make(map[string]interface{})
 			var encodedVal interface{}
@@ -136,11 +138,11 @@ func replaceByteArraysWithPlaceholders(data interface{}, buffers [][]byte) (inte
 				f := rt.Field(i)
 				fmt.Printf("Encoding %v\n", f.Name)
 				// TODO: Add support for JSON annotations
-				encodedVal, buffers = replaceByteArraysWithPlaceholders(rv.Field(i).Interface(), buffers)
+				encodedVal, attachments = replaceByteArraysWithPlaceholders(rv.Field(i).Interface(), attachments)
 				encoded[f.Name] = encodedVal
 			}
 
-			return encoded, buffers
+			return encoded, attachments
 		}
 	}
 
@@ -176,13 +178,13 @@ func encodeAsString(m *Message, attachmentCount *int) ([][]byte, error) {
 }
 
 func encodeAsBinary(m *Message) ([][]byte, error) {
-	buffers := make([][]byte, 0)
+	attachments := make([][]byte, 0)
 
-	encodedData, buffers := replaceByteArraysWithPlaceholders(m.Data, buffers)
+	encodedData, attachments := replaceByteArraysWithPlaceholders(m.Data, attachments)
 
 	m.Data = encodedData
 
-	attachmentCount := len(buffers)
+	attachmentCount := len(attachments)
 
 	encoded, err := encodeAsString(m, &attachmentCount)
 
@@ -190,11 +192,11 @@ func encodeAsBinary(m *Message) ([][]byte, error) {
 		return nil, err
 	}
 
-	buffersAndPacket := make([][]byte, attachmentCount+1)
-	buffersAndPacket[0] = encoded[0]
-	copy(buffersAndPacket[1:], buffers)
+	attachmentsAndPacket := make([][]byte, attachmentCount+1)
+	attachmentsAndPacket[0] = encoded[0]
+	copy(attachmentsAndPacket[1:], attachments)
 
-	return buffersAndPacket, nil
+	return attachmentsAndPacket, nil
 }
 
 // Encode encodes a Message into an array of []byte buffers
