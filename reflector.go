1bfd0e71133dcfcb758933d5db81a9e69eba496a
Alex Kube <alexander.j.kube@gmail.com>
2019-09-17 01:13:02 +0430

    Add reflect helper methods

---
+++ reflector.go
@@ -0,0 +1,77 @@
+package gocket
+
+import (
+	"errors"
+	"reflect"
+
+	"github.com/mitchellh/mapstructure"
+)
+
+func isFunction(handler interface{}) error {
+	if handler == nil {
+		return errors.New("Handler is nil")
+	}
+
+	v := reflect.ValueOf(handler)
+
+	if v.Kind() != reflect.Func {
+		return errors.New("Handler is not a function")
+	}
+
+	return nil
+}
+
+func convertUnmarshalledJSONToReflectValues(callable reflect.Value, data interface{}) ([]reflect.Value, error) {
+	if data == nil {
+		return nil, errors.New("Data is nil")
+	}
+
+	var callableData []reflect.Value
+
+	funcSignature := callable.Type()
+
+	// TODO: Make sure data isn't an array initially. For how this code is used, this won't be an issue
+	// because JSON.Unmarshal never returns fixed-length arrays
+	// It may, however, return a variety of single values e.g. "hello"; if that's the case,
+	// wrap them in an interface slice to simplify the code
+	switch data.(type) {
+	case []interface{}:
+	default:
+		data = []interface{}{data}
+	}
+
+	dataSlice := data.([]interface{})
+
+	for i := 0; i < funcSignature.NumIn(); i++ {
+		inType := funcSignature.In(i)
+
+		// If there's still data available in the input data, try use that to fill the callable array
+		if i < len(dataSlice) {
+			// If the parameter is a struct, try to use the json map[string]interface{} to populate its fields
+			if inType.Kind() == reflect.Struct {
+				// This is a pointer to a "zero" value for the type
+				val := reflect.New(inType).Interface()
+
+				err := mapstructure.Decode(dataSlice[i], val)
+
+				// If decoding fails, return an error
+				if err != nil {
+					return nil, err
+				}
+
+				// Append the decoded structure to the callable data
+				callableData = append(callableData, reflect.ValueOf(val).Elem())
+
+				continue
+			}
+
+			// If it's not a struct, just append the value
+			callableData = append(callableData, reflect.ValueOf(dataSlice[i]))
+		} else { // Otherwise pad the callable data with additional elements for the rest of the function parameters
+			val := reflect.New(inType).Elem().Interface()
+			callableData = append(callableData, reflect.ValueOf(val))
+		}
+	}
+
+	return callableData, nil
+}
---
+++ reflector_test.go
@@ -0,0 +1,88 @@
+package gocket
+
+import (
+	"reflect"
+	"testing"
+)
+
+type testUnmarshalType struct {
+	First  string
+	Second int
+}
+
+func TestIsFunction(t *testing.T) {
+	err := isFunction(nil)
+
+	if err == nil {
+		t.Error("Expected error, received none")
+	}
+
+	err = isFunction(5)
+
+	if err == nil {
+		t.Errorf("Expected error, received none")
+	}
+
+	fn := func(a string) {}
+
+	err = isFunction(fn)
+
+	if err != nil {
+		t.Errorf("Expected no error, received %v", err)
+	}
+}
+
+func TestConvertUnmarshalledJSONToReflectValues(t *testing.T) {
+	fn := func(a string) {}
+	fnVal := reflect.ValueOf(fn)
+
+	vals, err := convertUnmarshalledJSONToReflectValues(fnVal, "a")
+
+	if err != nil {
+		t.Errorf("Unable to convert %v", err)
+	}
+
+	if len(vals) != 1 {
+		t.Errorf("Expected 1 converted value, received %v", len(vals))
+	}
+
+	vals, err = convertUnmarshalledJSONToReflectValues(fnVal, []int{1, 2, 3})
+
+	if err != nil {
+		t.Errorf("Unable to convert %v", err)
+	}
+
+	if len(vals) != 1 {
+		t.Errorf("Expected 1 converted value, received %v", len(vals))
+	}
+
+	fnStruct := func(a testUnmarshalType) {}
+	fnVal = reflect.ValueOf(fnStruct)
+
+	vals, err = convertUnmarshalledJSONToReflectValues(fnVal, map[string]interface{}{
+		"first":  "the best",
+		"second": 5,
+	})
+
+	if err != nil {
+		t.Errorf("Unable to convert %v", err)
+	}
+
+	if len(vals) != 1 {
+		t.Errorf("Expected 1 converted value, received %v", len(vals))
+	}
+
+	switch first := vals[0].Interface().(type) {
+	case testUnmarshalType:
+		if first.First != "the best" {
+			t.Errorf("Expected .First to be 'the best', got %v", first.First)
+		}
+
+		if first.Second != 5 {
+			t.Errorf("Expected .Second to be 5, got %v", first.Second)
+		}
+	default:
+		t.Errorf("Expected testUnmarshalType, got %T", first)
+	}
+
+}
