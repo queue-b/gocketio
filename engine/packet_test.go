6c170adef219db920e6f2e0c68cd82ae9bccd969
Alex Kube <alexander.j.kube@gmail.com>
2019-10-22 19:15:44 +0430

    Parallelize tests

--- engine/packet_test.go
+++ engine/packet_test.go
@@ -2,105 +2,109 @@ package engine
 
 import "testing"
 
-func TestEncodeBinaryPacketAsString(t *testing.T) {
+func TestEncodeBinary(t *testing.T) {
 	packet := BinaryPacket{
 		Type: Message,
 		Data: []byte{0, 1, 2, 3, 4},
 	}
 
-	expected := "b4AAECAwQ="
+	t.Run("AsString", func(t *testing.T) {
+		t.Parallel()
+		expected := "b4AAECAwQ="
 
-	encoded, err := packet.Encode(false)
+		encoded, err := packet.Encode(false)
 
-	if err != nil {
-		t.Errorf("Unable to encode packet %v", err)
-	}
-
-	if string(encoded) != expected {
-		t.Errorf("Encoded packet invalid. Expected %v got %v", expected, encoded)
-	}
-}
-
-func TestEncodeBinaryPacketAsBinary(t *testing.T) {
-	packet := BinaryPacket{
-		Type: Message,
-		Data: []byte{0, 1, 2, 3, 4},
-	}
-
-	expected := []byte{byte(Message), 0, 1, 2, 3, 4}
-
-	encoded, err := packet.Encode(true)
-
-	if err != nil {
-		t.Errorf("Unable to encode packet %v", err)
-	}
-
-	if encoded == nil {
-		t.Error("Encoded packet invalid. Expected non-nil got nil")
-	}
-
-	if len(encoded) != len(expected) {
-		t.Errorf("Encoded packet invalid. Expected length %v got length %v", len(expected), len(encoded))
-	}
-
-	for i, v := range encoded {
-		if v != expected[i] {
-			t.Errorf("Encoded packet invalid. Expected %v at %v, got %v", expected[i], i, v)
+		if err != nil {
+			t.Errorf("Unable to encode packet %v", err)
 		}
-	}
+
+		if string(encoded) != expected {
+			t.Errorf("Encoded packet invalid. Expected %v got %v", expected, encoded)
+		}
+	})
+
+	t.Run("AsBinary", func(t *testing.T) {
+		t.Parallel()
+		expected := []byte{byte(Message), 0, 1, 2, 3, 4}
+
+		encoded, err := packet.Encode(true)
+
+		if err != nil {
+			t.Errorf("Unable to encode packet %v", err)
+		}
+
+		if encoded == nil {
+			t.Error("Encoded packet invalid. Expected non-nil got nil")
+		}
+
+		if len(encoded) != len(expected) {
+			t.Errorf("Encoded packet invalid. Expected length %v got length %v", len(expected), len(encoded))
+		}
+
+		for i, v := range encoded {
+			if v != expected[i] {
+				t.Errorf("Encoded packet invalid. Expected %v at %v, got %v", expected[i], i, v)
+			}
+		}
+	})
 }
 
-func TestEncodeStringPacketWithData(t *testing.T) {
-	data := "hello 亜"
+func TestEncodeString(t *testing.T) {
+	t.Run("WithData", func(t *testing.T) {
+		t.Parallel()
 
-	packet := StringPacket{
-		Type: Message,
-		Data: &data,
-	}
+		data := "hello 亜"
 
-	expected := "4hello 亜"
+		packet := StringPacket{
+			Type: Message,
+			Data: &data,
+		}
 
-	encoded, err := packet.Encode(false)
+		expected := "4hello 亜"
 
-	if err != nil {
-		t.Errorf("Unable to encode packet %v", err)
-	}
+		encoded, err := packet.Encode(false)
 
-	if encoded == nil {
-		t.Error("Encoded packet invalid. Expected non-nil got nil")
-	}
+		if err != nil {
+			t.Errorf("Unable to encode packet %v", err)
+		}
 
-	if len(encoded) != len(expected) {
-		t.Errorf("Encoded packet invalid. Expected length %v got length %v", len(expected), len(encoded))
-	}
+		if encoded == nil {
+			t.Error("Encoded packet invalid. Expected non-nil got nil")
+		}
 
-	if string(encoded) != expected {
-		t.Errorf("Encoded packet invalid. Expected %v, got %v", expected, string(encoded))
-	}
-}
+		if len(encoded) != len(expected) {
+			t.Errorf("Encoded packet invalid. Expected length %v got length %v", len(expected), len(encoded))
+		}
 
-func TestEncodeStringPacketWithoutData(t *testing.T) {
-	packet := StringPacket{
-		Type: Message,
-	}
+		if string(encoded) != expected {
+			t.Errorf("Encoded packet invalid. Expected %v, got %v", expected, string(encoded))
+		}
+	})
 
-	expected := "4"
+	t.Run("WithoutData", func(t *testing.T) {
+		t.Parallel()
+		packet := StringPacket{
+			Type: Message,
+		}
 
-	encoded, err := packet.Encode(false)
+		expected := "4"
 
-	if err != nil {
-		t.Errorf("Unable to encode packet %v", err)
-	}
+		encoded, err := packet.Encode(false)
 
-	if encoded == nil {
-		t.Error("Encoded packet invalid. Expected non-nil got nil")
-	}
+		if err != nil {
+			t.Errorf("Unable to encode packet %v", err)
+		}
 
-	if len(encoded) != len(expected) {
-		t.Errorf("Encoded packet invalid. Expected length %v got length %v", len(expected), len(encoded))
-	}
+		if encoded == nil {
+			t.Error("Encoded packet invalid. Expected non-nil got nil")
+		}
 
-	if string(encoded) != expected {
-		t.Errorf("Encoded packet invalid. Expected %v, got %v", expected, string(encoded))
-	}
+		if len(encoded) != len(expected) {
+			t.Errorf("Encoded packet invalid. Expected length %v got length %v", len(expected), len(encoded))
+		}
+
+		if string(encoded) != expected {
+			t.Errorf("Encoded packet invalid. Expected %v, got %v", expected, string(encoded))
+		}
+	})
 }
