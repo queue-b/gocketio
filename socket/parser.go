573352a065546f8def4f92dd5df83ff45d906168
Alex Kube <alexander.j.kube@gmail.com>
2019-10-27 00:36:48 +0430

    Add documentation for Encode

--- socket/parser.go
+++ socket/parser.go
@@ -9,35 +9,42 @@ import (
 type PacketType int
 
 const (
-	// Connect represents a packet that is sent when the remote connection is
+	// Connect is a packet that is sent when the remote connection is
 	// initially established
 	Connect PacketType = iota
-	// Disconnect represents a packet that is sent when the remote connection is broken
+	// Disconnect is a packet that is sent when the remote connection is broken
 	// either intentionally or unintentionally
 	Disconnect
-	// Event represents a packet that contains event information. Packets of this type
+	// Event is a packet that contains event information. Packets of this type
 	// may be sent by either side at any time after the connection is established
 	Event
-	// Ack represents a packet that contains a payload acknowledging receipt of a previously
+	// Ack is a packet that contains a payload acknowledging receipt of a previously
 	// sent Event
 	Ack
-	// Error represents a packet that contains error information
+	// Error is a packet that contains error information
 	Error
-	// BinaryEvent represents a packet that contains event information. Packets of this type
+	// BinaryEvent is a packet that contains event information. Packets of this type
 	// may be sent by either side at any time after the connection is established
 	BinaryEvent
-	// BinaryAck represents a packet that contains a payload acknowledging receipt of a previously
+	// BinaryAck is a packet that contains a payload acknowledging receipt of a previously
 	// sent Event
 	BinaryAck
 )
 
 // Packet is a Socket.IO packet
 type Packet struct {
-	Type            PacketType
-	ID              *int64
-	Namespace       string
+	// Type is the socket.io defined type of the packet
+	Type PacketType
+	// ID is set on outgoing Packets that require an Ack,
+	// and on incoming packets that Ack the receipt of a Packet.
+	ID *int64
+	// Namespace is used to define which sockets will receive the Packet
+	Namespace string
+	// AttachmentCount is the number of binary attachments that will be sent
+	// or received in addition to the main Packet
 	AttachmentCount int
-	Data            interface{}
+	// Data is the data contained in the packet
+	Data interface{}
 }
 
 func (p Packet) String() string {
@@ -121,7 +128,23 @@ func encodeAsBinary(p *Packet) ([][]byte, error) {
 	return attachmentsAndPacket, nil
 }
 
-// Encode encodes a Packet into an array of []byte buffers
+// Encode encodes a Packet into a format suitable for transmission
+// using an engine.io transport.
+// It returns a slice of byte slices which contain the socket.io encoded
+// contents of the packet.
+//
+// The length of the slice returned by Encode is always at least 1, unless an
+// error occurred during encoding. The exact length of the slice returned by Encode
+// is dependant on the value of the Type & Data fields of the packet and the value
+// of the supportsBinary argument. If supportsBinary is false, Encode returns a slice
+// with a single element. If supportsBinary is true, and Type is either BinaryEvent
+// or BinaryAck, Encode returns a slice with 1 + n additional elements, where
+// n is the total number of byte slices and byte arrays present in the Data
+// field, or are present at any level in any struct, array, or slice in the
+// Data field.
+//
+// The first element is the raw bytes of a string-encoded Socket.IO packet.
+// Additional elements are the raw bytes of a binary-encoded Socket.IO packet.
 func (p *Packet) Encode(supportsBinary bool) ([][]byte, error) {
 	if supportsBinary && p.Type == BinaryEvent || p.Type == BinaryAck {
 		return encodeAsBinary(p)
